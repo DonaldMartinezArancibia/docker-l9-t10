9bbb2c26171de60bc738fee691bec2e00756885b
DonaldMartinezArancibia <antonio.arancibia.18@gmail.com>
2022-08-03 14:55:44 -0600

    add in repo the ibex crud generator tool

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () { return view('welcome'); });
-Route::get('/', [App\Http\Controllers\RegistrosalidaController::class, 'pIndex']);
-
-Auth::routes();
-
-Route::resource('registrosalidas', App\Http\Controllers\RegistrosalidaController::class);
-Route::resource('herramientas', App\Http\Controllers\HerramientaController::class);
-Route::resource('empleados', App\Http\Controllers\EmpleadoController::class);
-Route::resource('categorias', App\Http\Controllers\CategoriaController::class);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
